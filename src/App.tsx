f22b8c4169fbacdf9addeae82b1613111222e6a3
Valeriia Zakharova <54675260+Zyxkim@users.noreply.github.com>
2026-02-01 21:35:09 +0300

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="grid grid-cols-3">
+        <h1>React</h1>
+        <h1>Tailwind</h1>
+        <h1>Vite</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
